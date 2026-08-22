b2d8d328ce61eb496b4c41ad8c04a98763172bab
Juan Sebastián Bedoya Castrillón <juansebastianbedoyacastrillon@MacBook-Air-de-Juan-Sebastian-B.local>
2026-08-22 16:11:36 -0500

    Update Instagram handle/link to @itsboomco

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 export const WHATSAPP_LINK =
   "https://wa.me/573053521932?text=" +
   encodeURIComponent("Hola!! Estoy interesado en los productos Its'Boom");
-export const INSTAGRAM_LINK = "https://www.instagram.com/itsboomcol/";
-export const INSTAGRAM_HANDLE = "@itsboomcol";
+export const INSTAGRAM_LINK = "https://www.instagram.com/itsboomco/";
+export const INSTAGRAM_HANDLE = "@itsboomco";
