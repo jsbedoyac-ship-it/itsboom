8793ddb130e894cc1ec48a2b86ffbd6c121f35be
Juan Sebastián Bedoya Castrillón <juansebastianbedoyacastrillon@MacBook-Air-de-Juan-Sebastian-B.local>
2026-08-22 15:38:12 -0500

    Point all WhatsApp CTAs to the new number 3053521932

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const WHATSAPP_LINK =
-  "https://wa.me/573332865715?text=" +
+  "https://wa.me/573053521932?text=" +
   encodeURIComponent("Hola!! Estoy interesado en los productos Its'Boom");
 export const INSTAGRAM_LINK = "https://www.instagram.com/itsboomcol/";
 export const INSTAGRAM_HANDLE = "@itsboomcol";
